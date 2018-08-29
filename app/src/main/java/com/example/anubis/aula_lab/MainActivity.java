e78cb4f5d1f56797a847ba8f81a425c92a612803
lucazbrandao <lucazbrandao@gmail.com>
2018-08-29 19:59:46 -0300

    Criar intent para o professor

--- app/src/main/java/com/example/anubis/aula_lab/MainActivity.java
+++ app/src/main/java/com/example/anubis/aula_lab/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.anubis.aula_lab;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -9,7 +11,8 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     TextView nomecompleto;
-    Button getName;
+    Button btnProfessor;
+    Button btnStudent;
     EditText nome;
     EditText sobrenome;
 
@@ -18,8 +21,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
+        btnProfessor = (Button) findViewById(R.id.btn_professor);
+        btnProfessor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ProfessorActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
